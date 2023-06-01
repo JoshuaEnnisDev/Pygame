91ddfabadbcb16f18d61cb81ff3b942d0a1d4a84
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2023-06-01 12:43:08 -0500

    Add files via upload

---
+++ Turtle/main.py
@@ -0,0 +1,31 @@
+import turtle
+
+wn = turtle.Screen()
+wn.bgcolor("lightgreen")
+
+player = turtle.Turtle()
+player.color("blue")
+player.shape("triangle")
+player.penup()
+
+speed = 1
+
+while True:
+    player.forward(speed)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
